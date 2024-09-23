af31fc8b70cfaf5df0e2ef925e04885061861b05
Ewan Belshaw <ewanbelshaw@gmail.com>
2024-09-23 15:47:33 -0400

    added js and updated the index

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function hoverButton(element) {
+    element.style.background='#609fff';
+    element.style.color='#FCFFDA'
+}
+
+function hoverExit(element){
+    element.style.background='';
+    element.style.color=''
+
+}

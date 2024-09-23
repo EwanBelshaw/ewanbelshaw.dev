effb2e9e66810adf91e379dc60d3a0ee1eed59e7
Ewan Belshaw <ewanbelshaw@gmail.com>
2024-09-23 16:56:19 -0400

    Mobile support

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ function hoverExit(element){
     element.style.color=''
 
 }
+
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
